503af453f2a908466bc03f1e009a8a8fd3258b9e
Komal <komalnamah0@gmail.com>
2024-12-30 19:28:36 +0530

    first commit

---
+++ ForgitcodeDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ForgitcodeDemo
+//
+//  Created by Apple on 30/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        print("123")
+    }
+
+
+}
+
